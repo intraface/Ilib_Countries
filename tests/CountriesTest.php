4875fb3ba3c475944ac0a3eac3fcaf7b896051f2
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2009-07-08 09:41:25 +0000

    Added tests of translation

--- tests/CountriesTest.php
+++ tests/CountriesTest.php
@@ -2,6 +2,9 @@
 
 require_once 'PHPUnit/Framework.php';
 require_once '../src/Ilib/Countries.php';
+require_once '../src/Ilib/Countries/Translation.php';
+
+require_once 'Ilib/ClassLoader.php';
 
 class CountriesTest extends PHPUnit_Framework_TestCase 
 {
@@ -15,7 +18,8 @@ class CountriesTest extends PHPUnit_Framework_TestCase
         
     }
     
-    function testgetCountryByName() {
+    function testgetCountryByName() 
+    {
         
         $countries = new Ilib_Countries();
         
@@ -29,12 +33,37 @@ class CountriesTest extends PHPUnit_Framework_TestCase
         
     }
     
-    public function testGetCountriesByRegionName() {
+    public function testGetCountriesByRegionName() 
+    {
         $countries = new Ilib_Countries();
         
         $result = $countries->getCountriesByRegionName(array('Western Europe', 'Eastern Europe'));
         
         $this->assertEquals(52, count($result));
+    }
+    
+    public function testWithTranslationByGivingObject()
+    {
+        $translation = Ilib_Countries_Translation::factory();
+        $translation->setLang('da');
+        $countries = new Ilib_Countries('utf-8', $translation);
+        
+        $countries = $countries->getCountriesByRegionName('Western Europe');
+        
+        $this->assertEquals('Danmark', $countries['Denmark']);
+        
+    }
+    
+    public function testWithTranslationByGivingArray()
+    {
+        $translation = Ilib_Countries_Translation::factory();
+        $translation->setLang('da');
+        $countries = new Ilib_Countries('utf-8', array($translation, 'get'));
+        
+        $countries = $countries->getCountriesByRegionName('Western Europe');
+        
+        $this->assertEquals('Danmark', $countries['Denmark']);
+        
     }
 }
 ?>
